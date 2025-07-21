1e8ff80dfb5018dfe2591874eb7c32213d1ec51f
mirai8955 <mirai.neyanderu1212@gmail.com>
2025-07-21 23:11:42 +0900

    create doc file write module

--- rmma2/sub_agents/curator_agent/tools.py
+++ rmma2/sub_agents/curator_agent/tools.py
@@ -1,8 +1,30 @@
 from log.rmma_logger import get_logger
+import os
+
 
 logger = get_logger()
-class FileManager:
-    def __init__(self):
+current_dir = os.getcwd()
+def doc_write(filename: str, content: str):
+    """
+    ドキュメントを保存する関数
+    Args:
+        filename: 拡張子無しのファイルの名前
+        content: ドキュメントとして保存する内容
+    Return:
+        result: 保存された内容
+    """
+
+    doc_file = os.path.join(current_dir, f"{filename}.md")
+    
+    try:
+        with open(doc_file, 'w', encoding='utf-8') as f:
+            f.write(content)
+        return content
+    except Exception as e:
+        logger.error(f"ドキュメントの保存中にエラーが発生しました: {doc_file}, エラー: {str(e)}")
+        raise e
+    
+
         
 
         
